2e1c00acd864dc96cedf8c5c3ab78512e143a172
Pachi-Dev <pachi.claros@gmail.com>
2023-10-13 14:50:22 -0600

    update performance

--- src/components/Home/Slider.jsx
+++ src/components/Home/Slider.jsx
@@ -1,29 +1,26 @@
+import React from 'react'
 import Carousel from 'react-bootstrap/Carousel'
-import { useLanguage } from '../hooks/useLanguage'
 import { Link } from 'react-router-dom'
-import { useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
 
 export function Slider () {
-  const { stateLang } = useLanguage()
-
-  const videoSource = useMemo(() => {
-    return stateLang === 'en' ? '/SSM-ENG.mp4' : '/SSM-ESP.mp4'
-  }, [stateLang])
-
+  const { i18n } = useTranslation()
   return (
     <div className='position-relative mt-5'>
       <Carousel fade interval={5000} controls={false}>
         <Carousel.Item>
-          <video className='w-100' muted autoPlay loop playsInline preload='auto'>
-            <source src={videoSource} type='video/mp4' />
-            Your browser does not support the video tag.
-          </video>
+          {
+          i18n.language === 'en'
+            ? <video className='w-100' muted autoPlay loop playsInline><source src='/SSM-ENG.mp4' type='video/mp4' />Your browser does not support the video tag.</video>
+            : <video className='w-100' muted autoPlay loop playsInline><source src='/SSM-ESP.mp4' type='video/mp4' />Your browser does not support the video tag.</video>
+          }
+
         </Carousel.Item>
         <Carousel.Item>
           <Link to='/energy-night'>
             <img
               className='d-block w-100'
-              src={stateLang === 'en' ? '/banner2_eng.webp' : '/banner2_esp.webp'}
+              src={i18n.language === 'en' ? '/banner2_eng.webp' : '/banner2_esp.webp'}
               alt='energy night mexico'
               loading='lazy'
             />

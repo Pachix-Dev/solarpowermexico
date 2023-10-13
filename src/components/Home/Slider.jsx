3e5255e6e8c78cba4069f7c95ed19b260202a4bb
Pachi-Dev <pachi.claros@gmail.com>
2023-10-13 14:55:33 -0600

    fix solar banner

--- src/components/Home/Slider.jsx
+++ src/components/Home/Slider.jsx
@@ -9,14 +9,6 @@ export function Slider () {
     <div className='position-relative mt-5'>
       <Carousel fade interval={5000} controls={false}>
         <Carousel.Item>
-          {
-          i18n.language === 'en'
-            ? <video className='w-100' muted autoPlay loop playsInline><source src='/SSM-ENG.mp4' type='video/mp4' />Your browser does not support the video tag.</video>
-            : <video className='w-100' muted autoPlay loop playsInline><source src='/SSM-ESP.mp4' type='video/mp4' />Your browser does not support the video tag.</video>
-          }
-
-        </Carousel.Item>
-        <Carousel.Item>
           <Link to='/energy-night'>
             <img
               className='d-block w-100'
@@ -25,6 +17,14 @@ export function Slider () {
               loading='lazy'
             />
           </Link>
+        </Carousel.Item>
+        <Carousel.Item>
+          {
+          i18n.language === 'en'
+            ? <video className='w-100' muted autoPlay loop playsInline><source src='/SSM-ENG.mp4' type='video/mp4' />Your browser does not support the video tag.</video>
+            : <video className='w-100' muted autoPlay loop playsInline><source src='/SSM-ESP.mp4' type='video/mp4' />Your browser does not support the video tag.</video>
+          }
+
         </Carousel.Item>
       </Carousel>
     </div>

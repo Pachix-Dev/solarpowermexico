dbf71720212071ab9fb151d45a5739450681e714
Pachi-Dev <pachi.claros@gmail.com>
2023-10-13 15:22:48 -0600

    UPDATE PERFORMANCE

--- src/components/Home/Gallery.jsx
+++ src/components/Home/Gallery.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import LightGallery from 'lightgallery/react'
 import 'lightgallery/css/lightgallery.css'
 import 'lightgallery/css/lg-zoom.css'
@@ -5,116 +6,35 @@ import 'lightgallery/css/lg-thumbnail.css'
 import lgThumbnail from 'lightgallery/plugins/thumbnail'
 import lgZoom from 'lightgallery/plugins/zoom'
 
-export function Gallery () {
+const images = [
+  { src: '/gallery/gallery1.webp', width: 300, height: 200 },
+  { src: '/gallery/gallery2.webp', width: 300, height: 200 },
+  { src: '/gallery/gallery3.webp', width: 300, height: 200 },
+  { src: '/gallery/gallery4.webp', width: 300, height: 200 },
+  { src: '/gallery/gallery5.webp', width: 300, height: 200 },
+  { src: '/gallery/gallery6.webp', width: 300, height: 200 },
+  { src: '/gallery/gallery7.webp', width: 300, height: 200 },
+  { src: '/gallery/gallery8.webp', width: 300, height: 200 }
+
+]
+
+const Gallery = () => {
   return (
-    <LightGallery
-      speed={500}
-      plugins={[lgThumbnail, lgZoom]}
-    >
-      <figure
-        data-src='/gallery/gallery1.webp'
-        className='gallery-item w-100 h-100'
-      >
-        <img
-          src='/gallery/gallery1.webp'
-          className='w-100 img-gallery'
-          width={312}
-          height={208}
-          loading='lazy'
-          alt='Energy Night 2023'
-        />
-      </figure>
-      <figure
-        data-src='/gallery/gallery2.webp'
-        className='gallery-item'
-      >
-        <img
-          src='/gallery/gallery2.webp'
-          className='img-gallery'
-          width={300}
-          height={200}
-          loading='lazy'
-          alt='Energy Night 2023'
-        />
-      </figure>
-      <figure
-        data-src='/gallery/gallery3.webp'
-        className='gallery-item'
-      >
-        <img
-          src='/gallery/gallery3.webp'
-          className='img-gallery'
-          width={300}
-          height={200.30}
-          loading='lazy'
-          alt='Energy Night 2023'
-        />
-      </figure>
-      <figure
-        data-src='/gallery/gallery4.webp'
-        className='gallery-item'
-      >
-        <img
-          src='/gallery/gallery4.webp'
-          className='img-gallery'
-          width={300}
-          height={200.30}
-          loading='lazy'
-          alt='Energy Night 2023'
-        />
-      </figure>
-      <figure
-        data-src='/gallery/gallery5.webp'
-        className='gallery-item'
-      >
-        <img
-          src='/gallery/gallery5.webp'
-          className='img-gallery'
-          width={300}
-          height={200.30}
-          loading='lazy'
-          alt='Energy Night 2023'
-        />
-      </figure>
-      <figure
-        data-src='/gallery/gallery6.webp'
-        className='gallery-item'
-      >
-        <img
-          src='/gallery/gallery6.webp'
-          className='img-gallery'
-          width={300}
-          height={200.30}
-          loading='lazy'
-          alt='Energy Night 2023'
-        />
-      </figure>
-      <figure
-        data-src='/gallery/gallery7.webp'
-        className='gallery-item'
-      >
-        <img
-          src='/gallery/gallery7.webp'
-          className='img-gallery'
-          width={300}
-          height={200}
-          loading='lazy'
-          alt='Energy Night 2023'
-        />
-      </figure>
-      <figure
-        data-src='/gallery/gallery8.webp'
-        className='gallery-item'
-      >
-        <img
-          src='/gallery/gallery8.webp'
-          className='img-gallery'
-          width={300}
-          height={168}
-          loading='lazy'
-          alt='Energy Night 2023'
-        />
-      </figure>
+    <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
+      {images.map((image, index) => (
+        <figure key={index} data-src={image.src} className='gallery-item'>
+          <img
+            src={image.src}
+            className='img-gallery'
+            width={image.width}
+            height={image.height}
+            loading='lazy'
+            alt='Energy Night 2023'
+          />
+        </figure>
+      ))}
     </LightGallery>
   )
 }
+
+export { Gallery }

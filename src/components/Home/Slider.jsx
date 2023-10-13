4785a2c9169d1f2f247a75f179fe394883d7f14b
Pachi-Dev <pachi.claros@gmail.com>
2023-10-13 13:10:05 -0600

    get more performance

--- src/components/Home/Slider.jsx
+++ src/components/Home/Slider.jsx
@@ -1,36 +1,32 @@
 import Carousel from 'react-bootstrap/Carousel'
 import { useLanguage } from '../hooks/useLanguage'
 import { Link } from 'react-router-dom'
+import { useMemo } from 'react'
 
 export function Slider () {
   const { stateLang } = useLanguage()
+
+  const videoSource = useMemo(() => {
+    return stateLang === 'en' ? '/SSM-ENG.mp4' : '/SSM-ESP.mp4'
+  }, [stateLang])
+
   return (
     <div className='position-relative mt-5'>
       <Carousel fade interval={5000} controls={false}>
         <Carousel.Item>
-          {
-            stateLang === 'en'
-              ? <video className='w-100' muted autoPlay loop playsInline src='/SSM-ENG.mp4' preload='auto' />
-              : <video className='w-100' muted autoPlay loop playsInline src='/SSM-ESP.mp4' preload='auto' />
-            }
+          <video className='w-100' muted autoPlay loop playsInline preload='auto'>
+            <source src={videoSource} type='video/mp4' />
+            Your browser does not support the video tag.
+          </video>
         </Carousel.Item>
         <Carousel.Item>
           <Link to='/energy-night'>
-            {
-            stateLang === 'en'
-              ? <img
-                  className='d-block w-100'
-                  src='/banner2_eng.webp'
-                  alt='energy night mexico'
-                  loading='lazy'
-                />
-              : <img
-                  className='d-block w-100'
-                  src='/banner2_esp.webp'
-                  alt='energy night mexico'
-                  loading='lazy'
-                />
-            }
+            <img
+              className='d-block w-100'
+              src={stateLang === 'en' ? '/banner2_eng.webp' : '/banner2_esp.webp'}
+              alt='energy night mexico'
+              loading='lazy'
+            />
           </Link>
         </Carousel.Item>
       </Carousel>

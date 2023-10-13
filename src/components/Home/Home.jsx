8ec3bf4b33d0ae0cda49fbaf89c5a74625d7cbc0
Pachi-Dev <pachi.claros@gmail.com>
2023-10-13 11:59:51 -0600

    update link floor

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -45,7 +45,7 @@ export function Home () {
               </svg>
               Factsheet 2024
             </a>
-            <a href='https://spmx24.mapyourshow.com/8_0/floorplan/index.cfm' target='_blank' rel='noreferrer'>
+            <a href='https://spmx24.mapyourshow.com/8_0/exhview/index.cfm' target='_blank' rel='noreferrer'>
               <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' width={30}>
                 <path strokeLinecap='round' strokeLinejoin='round' d='M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 01-1.125-1.125v-3.75zM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-8.25zM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-2.25z' />
               </svg> {t('home.floorplan')}

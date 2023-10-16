0d8a1c058952fbcfb7b0b666f73daeed0b4ea0cb
Pachi-Dev <pachi.claros@gmail.com>
2023-10-16 10:52:04 -0600

    hight performance

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,6 +5,7 @@ import { Gallery } from './Gallery'
 import { useTranslation } from 'react-i18next'
 import Marquee from 'react-fast-marquee'
 import { Link } from 'react-router-dom'
+import { Header } from './Header'
 export function Home () {
   const { t, i18n } = useTranslation()
 
@@ -42,64 +43,7 @@ export function Home () {
   return (
     <>
       <Container>
-        <header>
-          <nav className='mt-5 home-solar-submenu'>
-            <Link
-              to='/enlightenment-area'
-            >
-              <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' width={40}>
-                <path strokeLinecap='round' strokeLinejoin='round' d='M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z' />
-              </svg>
-              {t('home.program')}
-            </Link>
-            <a
-              href={i18n.language === 'en'
-                ? '/files/ENG-SPONSORSHIPS-SSM-v4.pdf'
-                : '/files/PATROCINIOS-SSM-2024-V4.pdf'}
-              target='_blank' rel='noreferrer'
-            >
-              <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' width={50}>
-                <path strokeLinecap='round' strokeLinejoin='round' d='M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z' />
-              </svg>
-              {t('home.sponsorship')}
-            </a>
-            <a
-              href={i18n.language === 'en'
-                ? '/files/RESULTS-REPORT-SSM-2023-ENG.pdf'
-                : '/files/REPORTE-DE-RESULTADOS-SSM-2023-ESP.pdf'}
-              target='_blank' rel='noreferrer'
-            >
-              <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' width={30}>
-                <path strokeLinecap='round' strokeLinejoin='round' d='M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6' />
-              </svg>
-              {t('home.results')}
-            </a>
-            <a
-              href={i18n.language === 'en'
-                ? '/files/SOLAR-STORAGE-MEXICO-FACTSHEET-INGLÉS.pdf'
-                : '/files/SOLAR-STORAGE-MEXICO-FACTSHEET-ESPAÑOL.pdf'}
-              target='_blank' rel='noreferrer'
-            >
-              <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' width={30}>
-                <path strokeLinecap='round' strokeLinejoin='round' d='M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z' />
-              </svg>
-              Factsheet 2024
-            </a>
-            <a href='https://spmx24.mapyourshow.com/8_0/exhview/index.cfm' target='_blank' rel='noreferrer'>
-              <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' width={30}>
-                <path strokeLinecap='round' strokeLinejoin='round' d='M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 01-1.125-1.125v-3.75zM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-8.25zM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-2.25z' />
-              </svg> {t('home.floorplan')}
-            </a>
-            <a href='/files/Directorio-SSM-2023.pdf' target='_blank' rel='noreferrer'>
-              <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' width={30}>
-                <path strokeLinecap='round' strokeLinejoin='round' d='M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25' />
-              </svg> {t('home.exhibitor_directory')}
-            </a>
-            <a href='https://hfmexico.mx/ecomondo/' aria-label='ecomondo' style={{ padding: 0, background: 'none' }} target='_blank' rel='noreferrer'>
-              <img width={150} height={57} src='/botone-ECO-esp.jpg' alt='ecomondo' />
-            </a>
-          </nav>
-        </header>
+        <Header />
       </Container>
       <Slider />
       <Container>

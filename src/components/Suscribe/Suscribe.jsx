7ad8d3ca3b49aa7d25f646e839b9fce587afd6b6
Pachi-Dev <pachi.claros@gmail.com>
2023-10-16 09:10:04 -0600

    optimize

--- src/components/Suscribe/Suscribe.jsx
+++ src/components/Suscribe/Suscribe.jsx
@@ -1,10 +1,8 @@
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
+import React, { Suspense, lazy, useRef, useState } from 'react'
+import { Col, Row, Form, Button } from 'react-bootstrap'
 import { useLanguage } from '../hooks/useLanguage'
 import { useTranslation } from 'react-i18next'
 
-import { Suspense, lazy, useRef, useState } from 'react'
-import { Col, Row } from 'react-bootstrap'
 const ReCAPTCHA = lazy(() => import('react-google-recaptcha'))
 
 export function Subscribe () {
@@ -13,20 +11,25 @@ export function Subscribe () {
   const captchaRef = useRef()
 
   const [captcha, setCaptcha] = useState(false)
-  const [message, setMessage] = useState()
+  const [message, setMessage] = useState('')
+
   const onChange = () => {
     setCaptcha(true)
   }
 
   const handleSubmit = async (event) => {
     event.preventDefault()
-    if (captcha === false) {
-      event.stopPropagation()
-      setMessage('Please verify you are not bot.')
-    } else {
+
+    try {
+      if (!captcha) {
+        setMessage('Please verify that you are not a bot.')
+        return
+      }
+
       const token = captchaRef.current.getValue()
       captchaRef.current.reset()
       setCaptcha(false)
+
       const formData = Object.fromEntries(new window.FormData(event.target))
       const requestOptions = {
         method: 'POST',
@@ -36,21 +39,19 @@ export function Subscribe () {
         body: JSON.stringify({ token, formData })
       }
 
-      try {
-        const res = await fetch(
-          'https://solarpowermexico.mx/newsletter/suscribe.php',
-          requestOptions
-        )
-        const data = await res.json()
-        if (data?.status === true || data === '23000') {
-          setMessage('Your are subscribe now!!')
-        } else {
-          setMessage('Sorry we couldn\'t verify you are not robot try again...')
-        }
-      } catch (error) {
-        console.log(error)
+      const res = await fetch('https://solarpowermexico.mx/newsletter/suscribe.php', requestOptions)
+      const data = await res.json()
+
+      if (data?.status === true || data === '23000') {
+        setMessage('You are subscribed now!')
+      } else {
+        setMessage("Sorry, we couldn't verify that you are not a robot. Please try again.")
       }
+
       document.getElementById('form-newsletter').reset()
+    } catch (error) {
+      console.error('Error:', error)
+      setMessage('An error occurred while processing your request. Please try again later.')
     }
   }
 
@@ -67,19 +68,18 @@ export function Subscribe () {
             <Form.Control type='email' name='email' required />
           </Form.Group>
           <Suspense fallback={<div>Loading reCAPTCHA...</div>}>
-            {
-            stateLang === 'en'
-              ? <><p className='d-none'>hola</p><ReCAPTCHA sitekey='6LeljqwnAAAAAHcToBhu6iq8o4kahL9sopQjC1A3' ref={captchaRef} onChange={onChange} hl='en' /></>
-              : <ReCAPTCHA
-                  sitekey='6LeljqwnAAAAAHcToBhu6iq8o4kahL9sopQjC1A3'
-                  ref={captchaRef}
-                  onChange={onChange}
-                  hl='es'
-                />
-
-              }
+            {stateLang === 'en'
+              ? (
+                <>
+                  <p className='d-none'>hola</p>
+                  <ReCAPTCHA sitekey='6LeljqwnAAAAAHcToBhu6iq8o4kahL9sopQjC1A3' ref={captchaRef} onChange={onChange} hl='en' />
+                </>
+                )
+              : (
+                <ReCAPTCHA sitekey='6LeljqwnAAAAAHcToBhu6iq8o4kahL9sopQjC1A3' ref={captchaRef} onChange={onChange} hl='es' />
+                )}
           </Suspense>
-          {captcha ? '' : <div style={{ color: '#dc3545' }}>{message}</div>}
+          <div style={{ color: '#dc3545' }}>{message}</div>
           <Button variant='dark' type='submit' className='mt-3'>
             {t('footer.subscription')}
           </Button>

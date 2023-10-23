4e4d6ea6c726ee92e788d990857b06718b7a159f
Pachi-Dev <pachi.claros@gmail.com>
2023-10-23 09:48:33 -0600

    update toolkit

--- src/components/Toolkit/Toolkit.jsx
+++ src/components/Toolkit/Toolkit.jsx
@@ -6,7 +6,10 @@ import 'lightgallery/css/lg-thumbnail.css'
 import lgThumbnail from 'lightgallery/plugins/thumbnail'
 import lgZoom from 'lightgallery/plugins/zoom'
 import './Toolkit.css'
+import { useTranslation } from 'react-i18next'
+
 export function Toolkit () {
+  const { i18n } = useTranslation()
   return (
     <>
       <section className='bg-secondary-text pt-5 pb-5 mt-5 mb-5'>
@@ -20,36 +23,35 @@ export function Toolkit () {
           speed={500}
           plugins={[lgThumbnail, lgZoom]}
         >
-
           <a
-            data-src='/LinkedIn_ENG_SPM2024.webp'
+            data-src={i18n.language === 'en' ? '/LinkedIn_ENG_SPM2024.webp' : '/LinkedIn_ESP_SPM2024.webp'}
             className='img-gallery'
           >
             <p className='text-center'>Linkedin</p>
             <img
-              src='/LinkedIn_ENG_SPM2024.webp'
+              src={i18n.language === 'en' ? '/LinkedIn_ENG_SPM2024.webp' : '/LinkedIn_ESP_SPM2024.webp'}
               className='w-100 p-2'
               loading='lazy'
             />
           </a>
           <a
-            data-src='/Twitter_SPM2024_ESP-scaled.webp'
+            data-src={i18n.language === 'en' ? 'Twitter_SPM2024_ESP-eng.webp' : '/Twitter_SPM2024_ESP-scaled.webp'}
             className='img-gallery'
           >
             <p className='text-center'>Twitter</p>
             <img
-              src='/Twitter_SPM2024_ESP-scaled.webp'
+              src={i18n.language === 'en' ? 'Twitter_SPM2024_ESP-eng.webp' : '/Twitter_SPM2024_ESP-scaled.webp'}
               className='w-100 p-2'
               loading='lazy'
             />
           </a>
           <a
-            data-src='/Facebook_ESP_SPM2024-scaled.webp'
+            data-src={i18n.language === 'en' ? 'Facebook_ENG_SPM2024.webp' : '/Facebook_ESP_SPM2024-scaled.webp'}
             className='img-gallery'
           >
             <p className='text-center'>Facebook</p>
             <img
-              src='/Facebook_ESP_SPM2024-scaled.webp'
+              src={i18n.language === 'en' ? 'Facebook_ENG_SPM2024.webp' : '/Facebook_ESP_SPM2024-scaled.webp'}
               className='w-100 p-2'
               loading='lazy'
             />
@@ -66,23 +68,23 @@ export function Toolkit () {
             />
           </a>
           <a
-            data-src='/Expositores-SPM2024.webp'
+            data-src={i18n.language === 'en' ? 'Expositores-SPM2024_ENG.webp' : '/Expositores-SPM2024.webp'}
             className='img-gallery'
           >
             <p className='text-center'>Expositor Confirmado</p>
             <img
-              src='/Expositores-SPM2024.webp'
+              src={i18n.language === 'en' ? 'Expositores-SPM2024_ENG.webp' : '/Expositores-SPM2024.webp'}
               className='w-100 p-2'
               loading='lazy'
             />
           </a>
           <a
-            data-src='/AccesoCortesia_SPM2024.webp'
+            data-src={i18n.language === 'en' ? 'AccesoCortesia_SPM2024_ENG.webp' : '/AccesoCortesia_SPM2024.webp'}
             className='img-gallery'
           >
             <p className='text-center'>Acceso de Cortesía</p>
             <img
-              src='/AccesoCortesia_SPM2024.webp'
+              src={i18n.language === 'en' ? 'AccesoCortesia_SPM2024_ENG.webp' : '/AccesoCortesia_SPM2024.webp'}
               className='w-100 p-2'
               loading='lazy'
             />

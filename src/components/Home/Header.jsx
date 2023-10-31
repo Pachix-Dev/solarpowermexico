166fc513faefd77d3a589f8ef6279f7377cef780
Pachi-Dev <pachi.claros@gmail.com>
2023-10-31 08:51:43 -0600

    update css and svg size

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -53,7 +53,7 @@ export function Header () {
           </svg> {t('home.floorplan')}
         </a>
         <a href='/files/Directorio-SSM-2023.pdf' target='_blank' rel='noreferrer'>
-          <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' width={30}>
+          <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' width={40}>
             <path strokeLinecap='round' strokeLinejoin='round' d='M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25' />
           </svg> {t('home.exhibitor_directory')}
         </a>

faad5eed20390df8bc12c9d9546e7899d87907f9
Pachi-Dev <pachi.claros@gmail.com>
2023-11-07 18:38:42 -0600

    Add exhibitors to home page and update translation.json

---
+++ src/constans_logos.js
@@ -0,0 +1,56 @@
+const partners = [
+  { link: 'https://www.energypartnership.mx/es/home/', image: '/AlianzaEnergetica.webp', width: 200, height: 67 },
+  { link: 'https://amsoc.mx/es/inicio/', image: '/80-amsoc.webp', width: 150, height: 98 },
+  { link: 'https://mexiko.ahk.de/es/', image: '/ahk.webp', width: 250, height: 65 },
+  { link: 'https://www.emergealliance.org/', image: '/emmerge.webp', width: 200, height: 85 },
+  { link: 'https://www.jalisco.gob.mx/', image: '/gobiernoJalisco.webp', width: 150, height: 150 },
+  { link: 'https://mermx.org/', image: '/merm.webp', width: 200, height: 103 }
+]
+const partners2 = [
+  { link: 'https://www.eupd-research.com/', image: '/EUPD-Research.webp', width: 200, height: 38 },
+  { link: 'http://www.galoenergy.com/', image: '/GaloEnergy.webp', width: 150, height: 76 },
+  { link: 'https://www.giz.de/en/worldwide/33041.html', image: '/giz.webp', width: 250, height: 108 },
+  { link: 'https://h2mex.org/', image: '/h2mexico-asoc.webp', width: 200, height: 68 },
+  { link: 'https://www.kas.de/es/web/mexiko', image: '/KAS.webp', width: 150, height: 46 },
+  { link: 'https://www.mujeresenergia.org/', image: '/redMujeres.webp', width: 200, height: 65 },
+  { link: 'https://www.solarenergy.org/', image: '/SEI.webp', width: 150, height: 107 }
+]
+const broadcasting = [
+  { link: 'https://www.eleconomista.com.mx/', image: '/economista.webp', width: 200, height: 19 },
+  { link: 'https://www.pv-magazine-mexico.com/', image: '/pv-magazine-group.webp', width: 200, height: 68 },
+  { link: 'https://mexicoindustry.com/es', image: '/mexicoIndustry.webp', width: 150, height: 53 },
+  { link: 'https://www.cosmos.com.mx/', image: '/onlineCosmos.webp', width: 200, height: 68 },
+  { link: 'https://energyandcommerce.com.mx/', image: '/Energy-Commerce.webp', width: 150, height: 60 },
+  { link: 'https://energiaadebate.com/', image: '/energiadebate.webp', width: 130, height: 75 },
+  { link: 'https://energiahoy.com/', image: '/EnergiaHoy.webp', width: 200, height: 45 },
+  { link: 'https://energy21.com.mx/', image: '/eneroy21.webp', width: 200, height: 40 }
+]
+const broadcasting2 = [
+  { link: 'https://globalenergy.mx/', image: '/Global-Energy.webp', width: 140, height: 62 },
+  { link: 'https://globalindustries.mx/', image: '/global-industries.webp', width: 200, height: 62 },
+  { link: 'https://greentology.life/', image: '/greentology.webp', width: 200, height: 55 },
+  { link: 'https://revistaconsultoria.com.mx/', image: '/revistaConsultoria.webp', width: 200, height: 50 },
+  { link: 'https://www.vanguardia-industrial.net/', image: '/VanguardiaIndustrial.webp', width: 150, height: 79 },
+  { link: 'https://worldconstruccion.mx/', image: '/worldConstruccion.webp', width: 200, height: 34 },
+  { link: 'https://discovercleantech.com/', image: '/discoverCleanTech.webp', width: 200, height: 48 },
+  { link: 'http://www.revistadp.com/', image: '/revista-de-preferencia.webp', width: 200, height: 57 },
+  { link: 'https://petroleoenergia.com/', image: '/petroleo_y_energia.webp', width: 150, height: 83 }
+]
+const exhibitors = [
+  { image: '/chint.webp', width: 200, height: 80 },
+  { image: '/SUNPOWER.webp', width: 200, height: 47 },
+  { image: '/bodega_solar.webp', width: 80, height: 117 },
+  { image: '/k2system.webp', width: 100, height: 100 },
+  { image: '/Soles.webp', width: 200, height: 60 },
+  { image: '/solaevergroup.webp', width: 200, height: 56 },
+  { image: '/apSystems.webp', width: 200, height: 50 },
+  { image: '/solargamo.webp', width: 200, height: 89 },
+  { image: '/weg.webp', width: 100, height: 69 },
+  { image: '/supportbysolarsteel.webp', width: 200, height: 105 },
+  { image: '/emmi.webp', width: 200, height: 69 },
+  { image: '/sma.webp', width: 150, height: 96 },
+  { image: '/solar_insurance.webp', width: 200, height: 113 },
+  { image: '/Fronius.webp', width: 200, height: 56 }
+]
+
+export { partners, partners2, broadcasting, broadcasting2, exhibitors }

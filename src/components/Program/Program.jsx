239b4e63e5ad4da4ce09464bc31a5f553f7e584d
Jesus Zermeño <josezerrod@hotmail.com>
2024-02-20 15:37:36 -0600

    se elimina codigo en comentarios

--- src/components/Program/Program.jsx
+++ src/components/Program/Program.jsx
@@ -68,21 +68,6 @@ export function Program() {
           >
             {programSolar?.april_18.map((program, index) => {
               return (
-                // <div key={index} className='program-date-item d-flex align-items-center'>
-                //   <div style={{ width: '200px' }}>
-                //     <p>
-                //       <svg width={20} xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor'>
-                //         <path strokeLinecap='round' strokeLinejoin='round' d='M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z' />
-                //       </svg> {program.duration}
-                //       <strong>{program.hour}</strong>
-                //     </p>
-                //   </div>
-                //   <div className='d-flex align-items-center justify-content-between w-100'>
-                //     <div className='w-100'>
-                //       <h2>{program.name}</h2>
-                //     </div>
-                //   </div>
-                // </div>
                 <Row className="align-items-center program-date-item">
                   <Col lg={2} xs={12} md={3} className="d-flex align-items-center">
                     <p>

f387c514e37017517c8b59d099f2af7d90cfca7d
Jesus Zermeño <josezerrod@hotmail.com>
2024-02-23 01:32:25 -0600

    se agrega fotografias y datos de speakers

---
+++ src/constans_speakers.js
@@ -0,0 +1,49 @@
+const speakers=[
+    {
+        id:1,
+        name: 'Angélica Soto',
+        avatar: '/speakers/angelica_soto.webp',
+        role_es: "Directora General Nx Buena Energía",
+        role_en: "General Manager Nx Buena Energía",
+        resume_es: 'Ha contribuido al crecimiento y consolidación del Grupo Nexus Energía, una empresa con más de 23 años de experiencia líder en gestión de activos renovables y una de las primeras comercializadoras de electricidad en el mercado liberalizado español.',
+        resume_en: 'She has contributed to the growth and consolidation of Grupo Nexus Energía, a company with more than 23 years of experience as a leader in asset management. 23 years of experience, leader in the management of renewable and one of the leading electricity traders in the Spanish deregulated market.',
+    },
+    {
+        id:2,
+        name: 'Valeria Amezcua',
+        avatar: '/speakers/valeria_amezcua.webp',
+        role_es: "Presidenta, Red Agrovoltaica Mexicana",
+        role_en: "President, Mexican Agrovoltaic Network",
+        resume_es: 'Con 9 años de experiencia en descarbonización y democratización de la energía, ha liderado el desarrollo y evaluación de regulaciones en la Comisión Reguladora de Energía. Actualmente, en VIA Climate Solutions, desarrolla estrategias de descarbonización, imparte cátedra sobre los Objetivos de Desarrollo Sostenible y preside la Red Agrovoltaica Mexicana, promoviendo la aplicación integral de la energía solar.',
+        resume_en: 'With 9 years of experience in decarbonization and energy democratization democratization of energy, he has led the development and evaluation of regulations at the Energy Regulatory Energy Regulatory Commission. Currently, at VIA Climate Solutions, he develops decarbonization strategies, teaches on the strategies, teaches on Sustainable Development Goals and chairs the Mexican Agrovoltaic Network, promoting the Agrovoltaic Network, promoting the integral application of solar energy.',
+    },
+    {
+        id:3,
+        name: 'Mario S. DePillis Jr.',
+        avatar: '/speakers/mario_pillis.webp',
+        role_es: "Experto en el mercado mayorista de la electricidad",
+        role_en: "Wholesale Electricity Market Competition Expert",
+        resume_es: 'Con más de 25 años de experiencia en el análisis y diseño de mercados eléctricos a nivel internacional. Durante su gestión en el Monitor Independiente del Mercado Eléctrico Mayorista en México, introdujo cambios que ahorraron 1.300 millones de dólares en el primer año. Además, ha contribuido significativamente en proyectos de investigación para el Departamento de Energía de los Estados Unidos.',
+        resume_en: 'With more than 25 years of experience in the analysis and design of international electricity markets. at an international level. During his tenure at the Independent Monitor of the Wholesale Electricity Mexico, he introduced changes that saved US$1.3 billion in the first year. 1.3 billion dollars in the first year. In addition, he has contributed significantly research projects for the U.S. Department of Energy.',
+    },
+    {
+        id:4,
+        name: 'Luis Gerardo Sánchez Stone',
+        avatar: '/speakers/luis_sanchez.webp',
+        role_es: "Director General en ErgoSolar",
+        role_en: "General Manager at ErgoSolar",
+        resume_es: 'Luis es el fundador de ErgoSolar, empresa líder en energía solar desde 2010. Ha sido reconocido por sus contribuciones en energía y diseño de dispositivos médicos patentados. Además, preside la comisión de energía en COPARMEX Puebla y es miembro fundador de asociaciones de energía solar. Su compromiso radica en desarrollar tecnología sostenible para México e internacionalmente.',
+        resume_en: 'Luis is the founder of ErgoSolar, a leading solar energy company since 2010. He has been recognized for his contributions in energy and patented medical device design. In addition, he chairs the energy commission at COPARMEX Puebla and is a founding member of solar energy associations. His commitment lies in developing sustainable technology for Mexico and internationally.',
+    },
+    {
+        id:5,
+        name: 'Enrique Garduño ',
+        avatar: '/speakers/enrique_gardunno.webp',
+        role_es: "CEO Skysense",
+        role_en: "CEO Skysense",
+        resume_es: 'Fundador y Director General de Skysense, ha impulsado más de 800 proyectos en operación. Su enfoque en soluciones fotovoltaicas y de almacenamiento, así como en sistemas híbridos y microrredes, ha dejado una huella significativa en la industria. Su compromiso con la promoción de prácticas empresariales responsables y sostenibles lo convierte en un referente destacado en el sector de la energía.',
+        resume_en: 'Founder and CEO of Skysense, he has driven more than 800 projects in operation. His focus on photovoltaic and storage solutions, as well as hybrid systems and microgrids, has left a significant footprint in the industry. His commitment to promoting responsible and sustainable business practices makes him a leading reference in the energy sector.',
+    },
+]
+export { speakers }
+  

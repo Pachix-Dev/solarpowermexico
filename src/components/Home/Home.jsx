63b8fb8e7a844e33a3937db5736bcfd12bb5f898
Jesus Zermeño <josezerrod@hotmail.com>
2024-02-27 21:06:38 -0600

    cambio en sección de testimoniales

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -339,7 +339,8 @@ export function Home() {
         </Marquee>
       </Container>
       <Container>
-        <div data-aos="fade-zoom-in" data-aos-duration="2000">
+        <div data-aos="fade-up"
+     data-aos-anchor-placement="top-bottom" data-aos-duration="1000">
           <p className='my-5 fw-bold fs-2 text-secondary text-center text-uppercase'>{t('home.title-testimonials')}</p>
           <Row className='mt-5 mb-5'>
             <Col lg={4} className='mt-3 text-center'>

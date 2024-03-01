bd74542ceb607061b773357962063ddc310bc29c
Jesus Zermeño <josezerrod@hotmail.com>
2024-03-01 13:19:32 -0600

    se ajusta imagen de banner

--- src/components/Home/ModalBanner.jsx
+++ src/components/Home/ModalBanner.jsx
@@ -1,22 +1,30 @@
 import { useState } from 'react'
 import Modal from 'react-bootstrap/Modal'
 import { Link } from 'react-router-dom'
+import './Home.css'
+import { Button } from 'bootstrap'
+import { Container } from 'react-bootstrap'
 
 export function ModalBanner() {
   const [modalShow, setModalShow] = useState(true)
+  const handleClose = () => setShow(false);
+
   return (
-    <Modal
-      show={modalShow}
-      size='lg'
-      aria-labelledby='contained-modal-title-vcenter'
-      centered
-      onHide={() => setModalShow(false)}
-    >
-      <Modal.Body className='m-0 p-0'>
-        <Link to='/conference-program'>
-          <img src="/banner_keynotes.webp" alt="" width='850' />
-        </Link>
-      </Modal.Body>
-    </Modal>
+    <div className=''>
+        <Modal
+          show={modalShow}
+          size='lg'
+          aria-labelledby='contained-modal-title-vcenter'
+          centered
+          onHide={() => setModalShow(false)}
+          className='container-banner'
+        >
+          <Modal.Body className='m-0 p-0'>
+            <Link to='/conference-program'>
+              <img src="/banner_keynotes.webp" alt="conferencias solar storage" width='100%' onClick={() => setModalShow(false)} />
+            </Link>
+          </Modal.Body>
+        </Modal>
+    </div>
   )
 }

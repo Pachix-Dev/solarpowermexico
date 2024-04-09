f1b65d29b4f9bae42ed4ab73c1ffc3451e2b8677
Jesus Zermeño <josezerrod@hotmail.com>
2024-04-09 17:29:48 -0600

    se actualiza maquetado de about-snec

--- src/components/AboutSNEC/AboutSNEC.jsx
+++ src/components/AboutSNEC/AboutSNEC.jsx
@@ -1,21 +1,26 @@
-import { Container } from 'react-bootstrap'
-import { useTranslation } from 'react-i18next'
+import { Container, Row, Col } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
 
-export function AboutSNEC () {
-  const { t } = useTranslation()
+export function AboutSNEC() {
+  const { t } = useTranslation();
   return (
-    <section className='mt-5 pt-5 pb-5 bg-secondary-text'>
+    <section className="mt-5 pt-5 pb-5 bg-secondary-text">
       <Container>
-        <div className='d-flex justify-content-between align-items-center'>
-          <p className='fw-bold fs-3'>
-            {t('AboutSNEC.title')}
-          </p>
-          <a href='http://www.snec.org.cn/' target='_blank' rel='noreferrer'>
-            <img src='/snecPower.webp' alt='repluslogo' width={163} height={124} />
-          </a>
-        </div>
-        <p className='mt-5' dangerouslySetInnerHTML={{ __html: t('AboutSNEC.description') }} />
+        <Row className="justify-content-md-center">
+          <Col className="text-center">
+            <p className="fw-bold fs-3">{t("AboutSNEC.title")}</p>
+          </Col>
+          <Col className="text-center">
+            <a href="http://www.snec.org.cn/" target="_blank" rel="noreferrer">
+              <img src="/snec-logo-new.webp" alt="repluslogo" height={124} />
+            </a>
+          </Col>
+        </Row>
+        <p
+          className="mt-5"
+          dangerouslySetInnerHTML={{ __html: t("AboutSNEC.description") }}
+        />
       </Container>
     </section>
-  )
+  );
 }

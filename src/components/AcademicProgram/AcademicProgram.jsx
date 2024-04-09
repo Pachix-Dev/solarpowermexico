131f90be2f8b1970168c631c7a36db0804f74f0a
Jesus Zermeño <josezerrod@hotmail.com>
2024-04-09 15:10:11 -0600

    se agrega descripción para ponencia Longi

--- src/components/AcademicProgram/AcademicProgram.jsx
+++ src/components/AcademicProgram/AcademicProgram.jsx
@@ -170,7 +170,7 @@ export function AcademicProgram() {
                                         </Col>
                                         <Col lg={8} xs={12} md={9}>
                                             <p className='text-start text-uppercase fs-2 fw-bold text-light'>{i18n.language === 'en' ? program?.title_en : program?.title}</p>
-                                            <p className='fs-5'>{program.name}</p>
+                                            <p className='fs-5 text-uppercase fw-bolder fst-italic'>{program.company}</p>
                                             <p className='text-position'>{i18n.language === 'en' ? program?.description_en : program?.description}</p>
                                             <p className='fs-5 fw-bold'>{program.speakers} <br /><span className='fw-normal fst-italic'>{i18n.language === 'en' ? program?.position_en : program?.position}</span></p>
                                             <div className='img-responsive'>
@@ -222,8 +222,7 @@ export function AcademicProgram() {
                                         </Col>
                                         <Col lg={8} xs={12} md={9}>
                                             <p className='text-start text-uppercase fs-2 fw-bold text-light'>{i18n.language === 'en' ? program?.title_en : program?.title}</p>
-                                            <p className='fs-5'>{program.name}</p>
-                                            <p className='text-position'>{i18n.language === 'en' ? program?.description_en : program?.description}</p>
+                                            <p className='fs-5 text-uppercase fw-bolder fst-italic'>{program.company}</p>                                            <p className='text-position'>{i18n.language === 'en' ? program?.description_en : program?.description}</p>
                                             <p className='fs-5 fw-bold'>{program.speakers} <br /><span className='fw-normal fst-italic'>{i18n.language === 'en' ? program?.position_en : program?.position}</span></p>
                                             <div className='img-responsive'>
                                                 {
